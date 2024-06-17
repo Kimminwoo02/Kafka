27e77dfddb2eb28e6dfc4332903f96dceb0fccc2
minutaurus <made_power@naver.com>
2024-06-17 15:22:04 +0900

    CircuitBreaker 튜닝

---
+++ src/main/java/com/example/kafkatest/config/Config.java
@@ -0,0 +1,21 @@
+package com.example.kafkatest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Resilience4jConfiguration{
+    @Bean
+    public Customizer<Resilince4JCircuitBreakerFactory> globalCustomConfiguration(){
+
+        CircuitBreakerConfig circuitBreakerConfig =CircuitBreakerConfig.custom()
+                .failureRateThreshold(4) // CircuitBReaker를 오픈할지 말지 결정하는 Failure rate, default : 50
+                .waitDurationInOpenState(Duration.ofMillis(1000)) //
+                .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
+                .slidingWindowSize(2)
+                .build();
+
+    }
+
+
+}
