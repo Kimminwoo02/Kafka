c576f58002771e35e989eac2ec2c11bef64b753c
minutaurus <made_power@naver.com>
2024-09-10 22:13:07 +0900

    Kafka Config 설정

--- src/main/java/com/example/kafkatest/config/Config.java
+++ src/main/java/com/example/kafkatest/config/Config.java
@@ -5,17 +5,17 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Resilience4jConfiguration{
-    @Bean
-    public Customizer<Resilince4JCircuitBreakerFactory> globalCustomConfiguration(){
-
-        CircuitBreakerConfig circuitBreakerConfig =CircuitBreakerConfig.custom()
-                .failureRateThreshold(4) // CircuitBReaker를 오픈할지 말지 결정하는 Failure rate, default : 50
-                .waitDurationInOpenState(Duration.ofMillis(1000)) //
-                .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
-                .slidingWindowSize(2)
-                .build();
-
-    }
+//    @Bean
+//    public Customizer<Resilince4JCircuitBreakerFactory> globalCustomConfiguration(){
+//
+//        CircuitBreakerConfig circuitBreakerConfig =CircuitBreakerConfig.custom()
+//                .failureRateThreshold(4) // CircuitBReaker를 오픈할지 말지 결정하는 Failure rate, default : 50
+//                .waitDurationInOpenState(Duration.ofMillis(1000)) //
+//                .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
+//                .slidingWindowSize(2)
+//                .build();
+//
+//    }
 
 
 }
